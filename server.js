5e91c683c5191ff4b865aefb2132969fc50791a8
DeeMichael <deemichaelb@gmail.com>
2016-04-03 17:33:28 -0600

    failing at recursion

--- server.js
+++ server.js
@@ -4,52 +4,42 @@ var request = require('request')
 var cheerio = require('cheerio')
 var app = express()
 
-var scrape = function(url){
-  app.get('/scrape', function(req, res) {
-    // url = 'http://www.amazon.com/b/ref=lp_3400371_ca_ln_1_0?node=1258595011&ie=UTF8&qid=1459544863'
-    // var json = { title : "", release : "", rating : ""}
-    request(url, function(error, response, html) {
-      if (!error) {
-        // console.log(html)
-        var $ = cheerio.load(html)
-        // var scrapeStuff = $('h1').text()
-        // for ( var i = 0; i < scrapeStuff.length; i++ ) {
-        //   console.log(scrapeStuff[i])
-        // }
-        $('a h2').filter(function(){
-          var data = $(this)
-          // console.log(data)
-          array = data.text().split('\n')
-          console.log(array)
-          // console.log(data.text())
-        })
+var scrape = function(url,num){
+    app.get('/scrape', function(req, res) {
+      // var json = { title : "", release : "", rating : ""}
+      request(url, function(error, response, html) {
+        if (!error) {
+          var $ = cheerio.load(html)
 
-      res.send(html)
-    }
+          $('a h2').filter(function(){
+            var data = $(this)
+            array = data.text().split('\n')
+            console.log(array)
+          })
+
+          var nextPageRoute = $('#pagnNextLink').attr('href')
+          var nextPageURL = 'http://www.amazon.com' + nextPageRoute
+          // res.send(html)
+          if (num >= 0){
+            console.log('Before -= num is: ', num)
+            num -= 1
+            console.log('After -= num is : ', num)
+            console.log('nextPageURL is: ', nextPageURL)
+            console.log(scrape(nextPageURL,num))
+            console.log('after recursive scrape call')
+          }
+          res.send("Check console!")
+        }
+      })
     })
-  })
-}
+  }
 var url = 'http://www.amazon.com/b/ref=lp_3400371_ca_ln_1_0?node=1258595011&ie=UTF8&qid=1459544863'
 
-scrape(url)
+// scrape(url,5)
 
-// app.get('/scrape', function(req, res) {
-//   url = 'http://www.amazon.com/b/ref=lp_3400371_ca_ln_1_0?node=1258595011&ie=UTF8&qid=1459544863'
-//   var json = { title : "", release : "", rating : ""};
-//
-//   request(url, function(error, response, html) {
-//     if (!error) {
-//       console.log(html)
-//       var $ = cheerio.load(html)
-//       var links = $('a')
-//       for ( var i = 0; i < links.length; i++ ) {
-//         console.log(links[i]);
-//       }
-//     res.send(html)
-//   }
-//   })
-// })
 
 app.listen('8081',function () {
   console.log("running on port 8081")
 })
+
+scrape(url,5)
