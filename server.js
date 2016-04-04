a3a7c4ce7b8c1a6687be33a7f856cd7d4c4a5e41
DeeMichael <deemichaelb@gmail.com>
2016-04-04 14:05:24 -0600

    works for one page

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ var request = require('request')
 var cheerio = require('cheerio')
 var app = express()
 
-var scrape = function(url,num){
-    app.get('/scrape', function(req, res) {
-      // var json = { title : "", release : "", rating : ""}
+var scrape = function(url, num) {
+  app.get('/scrape', function(req, res) {
+    // var json = { title : "", release : "", rating : ""}
+    var url = 'http://www.amazon.com/b/ref=lp_3400371_ca_ln_1_0?node=1258595011&ie=UTF8&qid=1459544863'
+    var num = 2
       request(url, function(error, response, html) {
         if (!error) {
           var $ = cheerio.load(html)
 
-          $('a h2').filter(function(){
+          $('a h2').filter(function() {
             var data = $(this)
             array = data.text().split('\n')
             console.log(array)
@@ -19,13 +21,13 @@ var scrape = function(url,num){
 
           var nextPageRoute = $('#pagnNextLink').attr('href')
           var nextPageURL = 'http://www.amazon.com' + nextPageRoute
-          // res.send(html)
-          if (num >= 0){
+            // res.send(html)
+          if (num >= 0) {
             console.log('Before -= num is: ', num)
             num -= 1
             console.log('After -= num is : ', num)
             console.log('nextPageURL is: ', nextPageURL)
-            console.log(scrape(nextPageURL,num))
+            console.log(scrape(nextPageURL, num))
             console.log('after recursive scrape call')
           }
           res.send("Check console!")
@@ -33,13 +35,13 @@ var scrape = function(url,num){
       })
     })
   }
-var url = 'http://www.amazon.com/b/ref=lp_3400371_ca_ln_1_0?node=1258595011&ie=UTF8&qid=1459544863'
 
-// scrape(url,5)
+var url = 'http://www.amazon.com/b/ref=lp_3400371_ca_ln_1_0?node=1258595011&ie=UTF8&qid=1459544863'
+scrape(url,5)
 
 
 app.listen('8081',function () {
   console.log("running on port 8081")
 })
 
-scrape(url,5)
+// scrape(url,5)
